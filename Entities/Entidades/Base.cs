d0b70484deb7569b2797d4a47340ad6fb9fcf1a2
Murilo Nascimento <murilohnascimento90murilohnascimento90@outlook.com.br>
2023-10-26 21:32:51 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Entidades/Base.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Notificacoes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Entidades
+{
+    public class Base : Notifica
+    {
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+        [Display(Name = "Name")]
+        public string Nome { get; set; }
+    }
+}
